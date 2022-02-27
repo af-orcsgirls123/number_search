e7b083a1356764b52647490fc2724352f778919f
Adriana <100041169+af-orcsgirls123@users.noreply.github.com>
2022-02-27 12:58:37 -0500

    Create workspace.js

---
+++ workspace.js
@@ -0,0 +1,9 @@
+function getNumberFact() {
+  var value = getText("Number");
+  var url = "http://numbersapi.com/"+value;
+  startWebRequest(url, updateFacts);
+}
+function updateFacts(status, type, content) {
+     setText("textarea", content);
+}
+onEvent("button1","click", getNumberFact);
